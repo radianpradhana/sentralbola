23f2c4132573f7f5973afadc4e36955537e67f12
unknown <radianpradhana@gmail.com>
2021-11-01 11:01:53 +0700

    ganti tampilan sesuai keinginan

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -7,13 +7,10 @@
   <!-- Content Header (Page header) -->
   <section class="content-header">
     <h1>
-      Fixed Layout
-      <small>Blank example to the fixed layout</small>
+      Beranda
     </h1>
     <ol class="breadcrumb">
-      <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-      <li><a href="#">Layout</a></li>
-      <li class="active">Fixed</li>
+      <li><a href="#"><i class="fa fa-dashboard"></i> Beranda</a></li>
     </ol>
   </section>
 
@@ -22,7 +19,7 @@
     <!-- Default box -->
     <div class="box">
       <div class="box-header with-border">
-        <h3 class="box-title">Title</h3>
+        <h3 class="box-title">SIPLAYER (Sistem Informasi Player)</h3>
 
         <div class="box-tools pull-right">
           <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
@@ -32,13 +29,9 @@
         </div>
       </div>
       <div class="box-body">
-        Start creating your amazing application!
+        Welcome di salah satu sistem informasi pemain bola terbesar di seluruh. . . . .impian Ryan =))
       </div>
       <!-- /.box-body -->
-      <div class="box-footer">
-        Footer
-      </div>
-      <!-- /.box-footer-->
     </div>
     <!-- /.box -->
 
